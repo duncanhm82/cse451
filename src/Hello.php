2087cd10b2d7984e2597091d0b68c4a07b0c7c52
duncanhm <duncanhm@miamioh.edu>
2022-05-04 09:26:53 -0400

    Initial commit

---
+++ src/Hello.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+ * (c) Hannah Duncan <duncanhm@miamioh.edu>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace liererkt\MyFirstPackage;
+
+class Hello
+{
+    public function say($name)
+    {
+        return "Hello, $name!";
+    }
+}
+
+?>
